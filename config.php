e4c672ed60e3e1328e3dc07db9f95cd326a19e12
AbigailRIMTETA <rimtetaabigail22@gmail.com>
2026-05-29 22:41:07 +0200

    Add database configuration in config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+// config.php
+$user = 'root';
+$password = 'root';
+$db = 'connecthub';
+$host = 'localhost';
+$port = 3306;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
